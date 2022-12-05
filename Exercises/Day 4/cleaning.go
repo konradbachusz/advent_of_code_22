8b6a8d14f461bf3872c594ffe52dec40f8da3818
konradbachusz <konradbachusz@gmail.com>
2022-12-05 19:51:01 +0000

    Part 2 solved

--- Exercises/Day 4/cleaning.go
+++ Exercises/Day 4/cleaning.go
@@ -10,19 +10,19 @@ import (
 	"strings"
 )
 
-// func intersection(s1, s2 []string) (inter []string) {
-// 	hash := make(map[string]bool)
-// 	for _, e := range s1 {
-// 		hash[e] = true
-// 	}
-// 	for _, e := range s2 {
-// 		// If elements present in the hashmap then append intersection list.
-// 		if hash[e] {
-// 			inter = append(inter, e)
-// 		}
-// 	}
-// 	return
-// }
+func intersection(s1, s2 []string) (inter []string) {
+	hash := make(map[string]bool)
+	for _, e := range s1 {
+		hash[e] = true
+	}
+	for _, e := range s2 {
+		// If elements present in the hashmap then append intersection list.
+		if hash[e] {
+			inter = append(inter, e)
+		}
+	}
+	return
+}
 
 func makeRange(min, max int) []int {
 	a := make([]int, max-min+1)
@@ -91,5 +91,51 @@ func main() {
 	}
 
 	fmt.Println("Part 1 solution:", count)
+
+	////////////////Part 2//////////////////
+
+	count = 0
+
+	for i, inventory := range pairs_list {
+		_ = inventory
+
+		//Split pair into separate into separate individuals
+		split_pair := strings.Split(pairs_list[i], ",")
+
+		elf1 := strings.Split(split_pair[0], "-")
+
+		elf2 := strings.Split(split_pair[1], "-")
+
+		slice1 := makeRange(str_to_int(elf1[0]), str_to_int(elf1[1]))
+		slice2 := makeRange(str_to_int(elf2[0]), str_to_int(elf2[1]))
+
+		// The int slice we are converting to a string.
+		slice1_txt := []string{}
+		slice2_txt := []string{}
+
+		// Create a string slice using strconv.Itoa.
+		// ... Append strings to it.
+		for i := range slice1 {
+			number := slice1[i]
+			text := strconv.Itoa(number)
+			slice1_txt = append(slice1_txt, text)
+		}
+
+		for i := range slice2 {
+			number := slice2[i]
+			text := strconv.Itoa(number)
+			slice2_txt = append(slice2_txt, text)
+		}
+
+		intersection_list := intersection(slice1_txt, slice2_txt)
+
+		//Check for slice intersection
+		if len(intersection_list) > 0 {
+			count = count + 1
+
+		}
+
+	}
+	fmt.Println("Part 2 solution:", count)
 
 }

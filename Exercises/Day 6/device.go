d09ce220c552b7801bda0ececc842c41baebc3e1
konradbachusz <konradbachusz@gmail.com>
2022-12-07 19:37:33 +0000

    Tidy up

--- Exercises/Day 6/device.go
+++ Exercises/Day 6/device.go
@@ -39,8 +39,6 @@ func main() {
 	//Reading string
 	s := string(content)
 
-	// _ = s
-	//test_str := "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"
 	window_string := ""
 	for i, character := range s {
 		_ = i
@@ -56,15 +54,11 @@ func main() {
 
 		char_count = char_count + 1
 
-		// fmt.Println("Character", string(character))
-		// fmt.Println("window_string:", string(window_string))
-
 	}
 	char_count = char_count + 1
 	fmt.Println("Part 1 Solution:", char_count)
 
 	/////////////Part 2///////////////
-	// test_str := "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"
 	window_string = ""
 	char_count = 0
 	for i, character := range s {
@@ -81,20 +75,8 @@ func main() {
 
 		char_count = char_count + 1
 
-		// fmt.Println("Character", string(character))
-		// fmt.Println("window_string:", string(window_string))
-
 	}
 	char_count = char_count + 1
 	fmt.Println("Part 2 Solution:", char_count)
 
 }
-
-/*Steps
-1. Read the data
-2. Initalize char_count
-3. Create a loop to iterate through characters
-4. Create a window of 4
-5. Check if characters in window are unique:
-	if no update char_count
-	If yes stop*/
